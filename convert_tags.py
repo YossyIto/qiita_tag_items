dc9a2c681fba4f19be6f3208d824816a1321d81d
YossyIto <ito5656@gmail.com>
2021-04-10 23:38:35 +0900

    v4 markdown変換追加

---
+++ convert_tags.py
@@ -0,0 +1,43 @@
+# jsonファイルに含まれる情報をもとにmarkdownファイルに変換する
+#
+#　likes_count [title](url)  
+
+# reference site
+# https://qiita.com/niwasawa/items/9b9b76d4f7ddb5426159 
+# https://qiita.com/kikuchiTakuya/items/53990fca06fb9ba1d8a7
+# https://www.tech-teacher.jp/blog/python-commandline/
+
+
+import sys
+import argparse  # for argparce
+import json
+
+# コマンドライン引数を取得
+parser = argparse.ArgumentParser( )
+parser.add_argument('arg1')
+args = parser.parse_args( )
+
+filename_in = args.arg1
+filename_out = filename_in + ".md"  # 拡張子を足すだけ
+
+# ファイルオープン
+print('********** JSONファイルを書き出す **********')
+json_open = open(filename_in, 'r')
+json_load = json.load(json_open)
+
+print( "item_count=" + str( len(json_load) ) )
+size = len(json_load)
+
+# 出力ファイル
+fw = open(filename_out,'w')
+    
+for x in range(0, size):
+  c = str( json_load[x]['likes_count'] ) 
+  c += ' [' + json_load[x]['title'] + ']'
+  c += '(' + json_load[x]['url'] + ')  '
+  fw.write( c +'\n' )
+
+# ファイルクローズ
+json_open.close()
+fw.close()
+# eof
---
+++ get_tags_v4_orien.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+# 日付を取得
+DATE_NOW=`date +%Y-%m-%dT`
+DATE_AGO_YEAR=`date -v -1y +%Y-%m-%dT`				#for Mac -dオプションが違う
+
+# 出力ファイル名を設定
+TAG_ID='新人教育'
+DATE_NOW=`date +%Y-%m-%dT`
+TAGS_FILE="tags_${TAG_ID}_${DATE_NOW}.json"
+RANKINGFILE="ranking_${TAG_ID}_${DATE_NOW}.json"
+echo "TAGS_FILE=${TAGS_FILE}"
+echo "RANKINGFILE=${RANKINGFILE}"
+
+# qiitaから対象タグの記事数を取得する
+curl "https://qiita.com/api/v2/tags/${TAG_ID}" | jq '.' > tags_detail.json
+ITEM_COUNT=$(jq '. | .items_count'  tags_detail.json)
+PAGE_COUNT=$(( $(( $ITEM_COUNT + 99)) /100 ))
+echo "ITEM_COUNT=${ITEM_COUNT}, PAGE_COUNT=${PAGE_COUNT}"
+
+# qiitaから対象タグの全記事を一覧取得する
+cp /dev/null ${TAGS_FILE}  # ファイルを空にしておく
+for (( i=1; i<=${PAGE_COUNT} ;i++ ))
+do
+  ss="curl 'https://qiita.com/api/v2/tags/${TAG_ID}/items?page=${i}&per_page=100' | jq '.[] | { likes_count: .likes_count, title: .title, url: .url}' >> ${TAGS_FILE}"
+  eval $ss
+done 
+
+# sort 降順
+jq 'sort_by(.likes_count) | reverse' --slurp ${TAGS_FILE}  > ${RANKINGFILE}
+
+# markdown ファイルに変換
+python convert_tags.py ${RANKINGFILE}
+# end
